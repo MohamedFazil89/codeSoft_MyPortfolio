b3b807a265484ec1263dd172347029c399d77d4b
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-01-27 23:50:41 +0530

    First commiting all files

---
+++ index.js
@@ -0,0 +1,17 @@
+function confirmDownload() {
+    // Display a confirmation dialog
+    var userConfirmation = confirm("Do you want to see my certificates file?");
+
+    // If the user clicks 'OK', initiate the download
+    if (userConfirmation) {
+        href = "./ilovepdf_merged.pdf";
+    }
+}
+
+const linkedin = document.getElementById("linkedin");
+const github = document.getElementById("github");
+const insta = document.getElementById("insta");
+// linked
+function linked(){
+    window.href = "www.github.com";
+}
