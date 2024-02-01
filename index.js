19f67c4d39dd2b523228d5a14f1c98a137f8aa83
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-02-01 16:41:48 +0530

    responsive code update

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function confirmDownload() {
-    // Display a confirmation dialog
     var userConfirmation = confirm("Do you want to see my certificates file?");
 
-    // If the user clicks 'OK', initiate the download
     if (userConfirmation) {
         href = "./ilovepdf_merged.pdf";
+    }else{
+        href = "#";
     }
 }
 
-const linkedin = document.getElementById("linkedin");
-const github = document.getElementById("github");
-const insta = document.getElementById("insta");
-// linked
-function linked(){
-    window.href = "www.github.com";
+
+//  scroll navbar style change
+var body = document.body;
+const navbar = document.getElementById("navbar");
+
+// function clicks(){
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+function handelscroll(){
+
+    var ScrollBody = document.body.scrollTop;
+    var  ScrollElement = document.documentElement.scrollTop;
+    if(ScrollBody === 0 &&  ScrollElement === 0){
+        navbar.style.backgroundColor = "";
+
+    }else{ 
+        navbar.style.backgroundColor = "rgba(0,0,0,0.5)";
+
+    }
+    body.addEventListener("scroll", handelscroll);
+
 }
+body.addEventListener("scroll", handelscroll);
+
+
+  
